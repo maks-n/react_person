395e884648001806b42bf14ed4d34ad2550526b4
Maksym Nazarenko <71183975+maks-n@users.noreply.github.com>
2024-03-30 14:19:41 -0400

    add ternaries and use destructuring

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,16 @@
 import './Person.scss';
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    {person.isMarried ? (
-      <>
-        {person.sex === 'm' && (
-          <p className="Person__partner">{person.partnerName} is my wife</p>
-        )}
-        {person.sex === 'f' && (
-          <p className="Person__partner">{person.partnerName} is my husband</p>
-        )}
-      </>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+  const partner = isMarried
+    ? `${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`
+    : 'I am not married';
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {age && <p className="Person__age">I am {age}</p>}
+      {<p className="Person__partner">{partner}</p>}
+    </section>
+  );
+};
